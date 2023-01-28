16a4249a8f35c2ab055cedbcc6b31f4f1ba04f45
lilacse <rirakusu@protonmail.com>
2023-01-28 23:56:13 +0800

    Only attempt to open http(s) origin urls

---
+++ fur/functions/open_remote.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+. "./utilities/handle_link.sh"
+
+# usage: fur remote [--show] 
+# opens or shows the repository's remote url.
+
+open_remote()
+{
+    cd "$FUR_PWD"
+
+    remote="$(git config --get remote.origin.url)"
+
+    if [ "$?" -eq "1" ]; then
+        echo "Origin URL not set for this repository."
+        return "$?"
+    fi
+
+    # only handle link if it is a http/https link
+
+    echo "$remote" | grep -Eq "^https?://"
+
+    if [ "$?" -eq "0" ]; then 
+        handle_link "$url"
+    else 
+        echo "Origin URL is not a http(s) link that can be opened: $remote"
+        return 1
+    fi
+}
