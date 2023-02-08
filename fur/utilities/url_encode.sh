06ce28be77a325351251413b8b0fa324973e7d4d
lilacse <rirakusu@protonmail.com>
2023-02-09 00:06:17 +0800

    `url_encode` utility script.

---
+++ fur/utilities/url_encode.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# usage: url_encode [string]
+
+url_encode()
+{
+    if [ "$#" -ne "1" ]; then 
+        echo "Error in url_encode: Can only accept 1 argument, received $# instead."
+        return 1
+    fi
+
+    echo "$1" | tr -d "\n" | xxd -p | sed 's/../%&/g'
+
+    return $?
+}
