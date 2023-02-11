87262041d594fe55dec56aef5063826eddd240e0
Lilacse <rirakusu@protonmail.com>
2023-02-11 11:38:30 +0800

    Underscore fix for `pull-requests` usage.

--- fur/functions/pull_requests.sh
+++ fur/functions/pull_requests.sh
@@ -3,14 +3,14 @@
 . "./utilities/handle_link.sh"
 . "./utilities/process_remote_url.sh"
 
-# usage: fur pull_requests 
+# usage: fur pull-requests 
 # opens the pull requests page for the repository on the remote's website.
 
 pull_requests()
 {
     if [ "$#" -ne 0 ]; then 
         echo "Invalid arguments."
-        echo "usage: fur pull_requests"
+        echo "usage: fur pull-requests"
         return 1
     fi
 
