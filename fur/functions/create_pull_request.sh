317d44986cb7e3e95cabe2b0b8da65e156f4a241
lilacse <rirakusu@protonmail.com>
2023-02-13 23:22:07 +0800

    Source and target repo overrides for `create-pull-request`.

--- fur/functions/create_pull_request.sh
+++ fur/functions/create_pull_request.sh
@@ -3,16 +3,34 @@
 . "./utilities/handle_link.sh"
 . "./utilities/process_remote_url.sh"
 
-# usage: fur create-pull-request 
+# usage: fur create-pull-request [--from source_repo] [--to target_repo]
 # opens the create pull request page for the repository on the remote's website.
 
 create_pull_request()
 {
-    if [ "$#" -ne 0 ]; then 
+    options=$(getopt -a -u --longoptions "from:,to:" -- "" "$@")
+
+    if [ "$?" -ne "0" ]; then 
+        echo
         echo "Invalid arguments."
-        echo "usage: fur create-pull-request"
+        echo "usage: fur create-pull-request [--from source_repo] [--to target_repo]"
         return 1
     fi
+
+    set -- $options
+
+    while true; do 
+        if [ "$1" = "--from" ]; then 
+            from_repo="$2"
+            shift 2
+        elif [ "$1" = "--to" ]; then 
+            to_repo="$2"
+            shift 2
+        elif [ "$1" = "--" ]; then 
+            shift
+            break
+        fi
+    done
 
     remote="$(git -C "$FUR_PWD" config --get remote.origin.url)"
 
@@ -25,12 +43,18 @@ create_pull_request()
 
     remote=$(process_remote_url "$remote")
 
+    # sets from_repo to current repo by default
+
+    if [ -z "$from_repo" ]; then 
+        from_repo=$(git -C "$FUR_PWD" symbolic-ref --short HEAD)
+    fi
+
     # handle GitHub repo
 
     echo "$remote" | grep -Eq "^https://github.com/.+$"
 
     if [ "$?" -eq "0" ]; then 
-        create_pr_page=$(printf "%s/compare" "$remote")
+        create_pr_page=$(printf "%s/compare/%s...%s" "$remote" "$to_repo" "$from_repo")
         handle_link "$create_pr_page"
         return $?
     fi
@@ -40,7 +64,7 @@ create_pull_request()
     echo "$remote" | grep -Eq "^https://dev.azure.com/.+/_git/.+$" 
 
     if [ "$?" -eq "0" ]; then 
-        create_pr_page=$(printf "%s/pullrequestcreate" "$remote")
+        create_pr_page=$(printf "%s/pullrequestcreate?sourceRef=%s&targetRef=%s" "$remote" "$from_repo" "$to_repo")
         handle_link "$create_pr_page"
         return $?
     fi
