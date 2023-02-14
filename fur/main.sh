602ae488c78facb427941f89cf4c1fb16b6440b0
lilacse <rirakusu@protonmail.com>
2023-02-14 22:50:42 +0800

    Replace if-else chain with case statement in main.sh

--- fur/main.sh
+++ fur/main.sh
@@ -10,44 +10,54 @@ main()
         echo "Folder \"$FUR_PWD\" is not a git repo!"
         exit 3
     fi
+    
 
     if [ "$#" -eq "0" ]; then 
         echo "No arguments supplied!"
         exit 1
     fi
 
-    if [ "$1" = "open-remote" ]; then 
-        . "./functions/open_remote.sh"
-        shift 1
-        open_remote "$@"
-        exit $?
-    elif [ "$1" = "recommit" ]; then 
-        . "./functions/recommit.sh"
-        shift 1
-        recommit "$@"
-        exit $?
-    elif [ "$1" = "pull-requests" ]; then 
-        . "./functions/pull_requests.sh"
-        shift 1
-        pull_requests "$@"
-        exit $?
-    elif [ "$1" = "create-pull-request" ]; then 
-        . "./functions/create_pull_request.sh"
-        shift 1
-        create_pull_request "$@"
-        exit $?
-    elif [ "$1" = "issues" ]; then 
-        . "./functions/issues.sh"
-        shift 1
-        issues "$@"
-        exit $?
-    elif [ "$1" = "issue" ]; then 
-        . "./functions/issue.sh"
-        shift 1
-        issues "$@"
-        exit $?
-    else 
-        echo "Option not understood: $1"
-        exit 2
-    fi
+
+    case "$1" in 
+        "open-remote")
+            . "./functions/open_remote.sh"
+            shift 1
+            open_remote "$@"
+            exit $?
+            ;;
+        "recommit")
+            . "./functions/recommit.sh"
+            shift 1
+            recommit "$@"
+            exit $?
+            ;;
+        "pull-requests")
+            . "./functions/pull_requests.sh"
+            shift 1
+            pull_requests "$@"
+            exit $?
+            ;;
+        "create-pull-request")
+            . "./functions/create_pull_request.sh"
+            shift 1
+            create_pull_request "$@"
+            exit $?
+            ;;
+        "issues")
+            . "./functions/issues.sh"
+            shift 1
+            issues "$@"
+            exit $?
+            ;;
+        "issue")
+            . "./functions/issue.sh"
+            shift 1
+            issues "$@"
+            exit $?
+            ;;
+        *)
+            echo "Option not understood: $1"
+            exit 2
+            ;;
+    esac
 }
