84974ddb5631b881dd150da6e8d556cd9556a6e0
lilacse <rirakusu@protonmail.com>
2023-02-18 17:05:14 +0800

    `cd` fail message in entry script.

--- fur.sh
+++ fur.sh
@@ -5,4 +5,7 @@ export FUR_PWD="$PWD"
 if cd "$(dirname "$(realpath "$0")")/"; then 
     . ./fur/main.sh
     main "$@"
+else 
+    echo "Failed to cd to fur's root directory." > /dev/stderr
+    exit 1
 fi
