68604e0c5245ff3c639900cc275cff2d4f5407a7
Lilacse <rirakusu@protonmail.com>
2023-05-28 15:36:25 +0800

    Fix stderr redirection when opening link on Linux

--- fur/link_handler/open_link.py
+++ fur/link_handler/open_link.py
@@ -1,8 +1,11 @@
+import subprocess
 from sys import platform
-import subprocess 
+
 
 def open_link(link: str):
-    if platform == 'linux':
-        subprocess.run(['xdg-open', link])
-    elif platform == 'win32':
-        subprocess.run(['start', link], shell=True)
+    if platform == "linux":
+        # for any reason, setting stderr to subprocess.DEVNULL does not properly 
+        # redirect all stderr output in this case. 
+        subprocess.run(f"2>/dev/null 1>&2 xdg-open '{link}'", shell=True)
+    elif platform == "win32":
+        subprocess.run(["start", link], shell=True)
