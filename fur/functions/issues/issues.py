2e2acb8a6ab4a0b3be09e8d282b9b487f0f5f0d0
Lilacse <rirakusu@protonmail.com>
2023-09-24 22:19:41 +0800

    Fix `tasks` alias for `issues`

--- fur/functions/issues/issues.py
+++ fur/functions/issues/issues.py
@@ -5,7 +5,7 @@ from link_handler import open_link
 
 
 def issues(args: []):
-    parser = argparse.ArgumentParser(prog="issues | task | bugs | tickets | work-items",
+    parser = argparse.ArgumentParser(prog="issues | tasks | bugs | tickets | work-items",
                                      description="opens the current repo's issues page in it's remote origin's website")
     parser.parse_args(args)
 
