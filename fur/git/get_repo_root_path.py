94440738d68b89f16f92572b488702871f7c0911
Lilacse <rirakusu@protonmail.com>
2023-12-06 21:50:25 +0800

    Fix getting repo root path in Elvish

--- fur/git/get_repo_root_path.py
+++ fur/git/get_repo_root_path.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 def get_repo_root_path() -> Path:
     git_command = subprocess.run(
-        ["git", "rev-parse", "--git-dir"], capture_output=True, text=True
+        ["git", "rev-parse", "--path-format=absolute", "--git-dir"], capture_output=True, text=True
     )
     if git_command.returncode == 0:
         git_dir_path = Path(git_command.stdout.strip())
